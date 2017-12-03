7521fcc0e9958bce4b34e319c5c3d98f97f9d4b0
이태웅 <dlxldnd@naver.com>
2017-12-03 20:16:35 +0900

    로그인 구현

--- app/src/main/java/com/example/taewoong/exchangestudent/Activity/LoginActivity.java
+++ app/src/main/java/com/example/taewoong/exchangestudent/Activity/LoginActivity.java
@@ -1,22 +1,39 @@
 package com.example.taewoong.exchangestudent.Activity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Html;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.taewoong.exchangestudent.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 /**
  * Created by Taewoong on 2017-11-17.
  */
 
 public class LoginActivity extends AppCompatActivity{
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    EditText password;
+    EditText id;
+    Button LoginBtn;
+    Button signup;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +45,79 @@ public class LoginActivity extends AppCompatActivity{
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayShowCustomEnabled(true);
         actionBar.setDisplayShowTitleEnabled(false);
+
+        password = (EditText)findViewById(R.id.password);
+        id = (EditText)findViewById(R.id.id);
+        LoginBtn = (Button)findViewById(R.id.login);
+        signup = (Button)findViewById(R.id.snbutton);
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Toast.makeText(LoginActivity.this, "Login singed in", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(LoginActivity.this, "Login signed out", Toast.LENGTH_SHORT).show();
+                }
+            }
+        };
+
+        LoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                clickSignIn();
+            }
+        });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    boolean isEmptyEditField(){
+        boolean empty = false;
+        if(password.getText().toString() == "" || id.getText().toString() == ""){
+            empty = true;
+        }
+        return empty;
+    }
+
+    void clickSignIn(){
+        if(isEmptyEditField()){
+            Toast.makeText(this, "You should fill fields", Toast.LENGTH_LONG).show();
+        }else{
+            mAuth.signInWithEmailAndPassword(id.getText().toString(), password.getText().toString())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(!task.isSuccessful()){
+                                Toast.makeText(LoginActivity.this, "SignIn Failed", Toast.LENGTH_SHORT).show();
+                            }else{
+                                Toast.makeText(LoginActivity.this, "SignIn Success", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if(mAuthListener != null){
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
