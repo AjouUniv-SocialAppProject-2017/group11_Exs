44ce030e263fc8dedcd2f4a10746e5e813a1627e
Wonseok <robinseok@gmail.com>
2017-12-10 03:35:56 +0900

    장르데이터

---
+++ app/src/main/java/com/example/taewoong/exchangestudent/Database/GenreData.java
@@ -0,0 +1,17 @@
+package com.example.taewoong.exchangestudent.Database;
+
+/**
+ * Created by wonseoklee on 2017. 12. 10..
+ */
+
+public class GenreData {
+
+    public String name;
+
+    public GenreData() {
+    }
+
+    public GenreData(String name) {
+        this.name = name;
+    }
+}
